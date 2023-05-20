98f2a353f8e84f31459411b720ac959b9c638fb4
Muhammad Athallah <mhd.athallah@gmail.com>
2023-05-20 20:23:48 +0700

    feat: simple socket.io server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import express from "express";
 import { createServer } from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer);
 
-io.on("connection", (socket) => {
-  // ...
+io.on("connection", (socket: Socket) => {
+  const userId = 'user-id'; // Replate with appropriate user ID
+  socket.join(userId);
 });
 
-httpServer.listen(3000);
+app.post('/send-data/:userId', (req, res) => {
+  const { userId } = req.params;
+  const { data } = req.body;
+
+  io.to(userId).emit('data', data);
+
+  res.sendStatus(200);
+});
+
+httpServer.listen(3000, () => {
+  console.log("Socket.IO server berjalan di http://localhost:3000");
+});
